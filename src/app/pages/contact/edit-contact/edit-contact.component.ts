f474ee3dc2c7409999c21850b0c0d2d8d076b133
ruddy palvair <r.palvair@gmail.com>
2021-04-21 12:41:22 +0200

    feat : Load contact by id

--- src/app/pages/contact/edit-contact/edit-contact.component.ts
+++ src/app/pages/contact/edit-contact/edit-contact.component.ts
@@ -1,6 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { config } from "../../../config";
 
 @Component({
   selector: 'app-edit-contact',
@@ -12,7 +14,8 @@ export class EditContactComponent implements OnInit {
   contactForm: FormGroup;
 
   constructor(private activatedRoute: ActivatedRoute,
-    private formBuilder: FormBuilder) {
+    private formBuilder: FormBuilder,
+    private httpClient: HttpClient) {
     this.contactForm = this.formBuilder.group(
       {
         firstName: ['', Validators.required],
@@ -23,11 +26,22 @@ export class EditContactComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    console.log("contact id", this.activatedRoute.snapshot.params.id)
+    this.loadContact()
   }
 
   onSubmit() {
-    console.log("submit",this.contactForm.value)
+    console.log("submit", this.contactForm.value)
   }
 
+  loadContact() {
+    let id = this.activatedRoute.snapshot.params.id
+    console.log("contact id", id)
+    this.httpClient.get(config.endpoints.contacts.read + "/" + id).subscribe((data: any) => {
+      this.contactForm.patchValue({
+        firstName: data.firstName,
+        lastName: data.lastName,
+        age: data.age
+      })
+    })
+  }
 }
