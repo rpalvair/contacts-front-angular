28d954c2a6039caf74310e1d58d7d13d279acd70
ruddy palvair <r.palvair@gmail.com>
2021-04-26 19:49:09 +0200

    feat: Remove footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TheFooterComponent } from './components/the-footer/the-footer.component';
 import { TheHeaderComponent } from './components/the-header/the-header.component';
 import { AddContactComponent } from './pages/contact/add-contact/add-contact.component';
 import { ContactsListComponent } from './pages/contact/contacts-list/contacts-list.component';
@@ -16,7 +15,6 @@ import { EditContactComponent } from './pages/contact/edit-contact/edit-contact.
   declarations: [
     AppComponent,
     TheHeaderComponent,
-    TheFooterComponent,
     AddContactComponent,
     EditContactComponent,
     ContactsListComponent
