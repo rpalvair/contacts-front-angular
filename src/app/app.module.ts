54df106dc006c9a9b724488edd2b6b4d1229d99a
ruddy palvair <r.palvair@gmail.com>
2021-04-26 14:40:23 +0200

    feat : Change home component to contacts list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,25 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TheHeaderComponent } from './components/the-header/the-header.component';
 import { TheFooterComponent } from './components/the-footer/the-footer.component';
-import { HomeComponent } from './pages/home/home.component';
-import { HttpClientModule } from '@angular/common/http';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TheHeaderComponent } from './components/the-header/the-header.component';
 import { AddContactComponent } from './pages/contact/add-contact/add-contact.component';
+import { ContactsListComponent } from './pages/contact/contacts-list/contacts-list.component';
 import { EditContactComponent } from './pages/contact/edit-contact/edit-contact.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     TheHeaderComponent,
     TheFooterComponent,
-    HomeComponent,
     AddContactComponent,
-    EditContactComponent
+    EditContactComponent,
+    ContactsListComponent
   ],
   imports: [
     BrowserModule,
--- src/app/pages/contact/contacts-list/contacts-list.component.spec.ts
+++ src/app/pages/contact/contacts-list/contacts-list.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomeComponent } from './home.component';
+import { ContactsListComponent } from './contacts-list.component';
 
-describe('HomeComponent', () => {
-  let component: HomeComponent;
-  let fixture: ComponentFixture<HomeComponent>;
+describe('ContactsListComponent', () => {
+  let component: ContactsListComponent;
+  let fixture: ComponentFixture<ContactsListComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ ContactsListComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomeComponent);
+    fixture = TestBed.createComponent(ContactsListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
